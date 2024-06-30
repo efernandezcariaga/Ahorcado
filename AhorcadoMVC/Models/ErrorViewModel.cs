bbc4a4213d80e8ae8aacd793033ee804e556aa86
GinoGallina <ginogallina2002@gmail.com>
2024-06-30 17:35:26 -0300

    avances de front

--- AhorcadoMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AhorcadoMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- AhorcadoMVC/Program.cs
+++ AhorcadoMVC/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,7 +16,20 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "Scripts")),
+    RequestPath = "/Scripts"
+});
+
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "Content")),
+    RequestPath = "/Content"
+});
 
 app.UseRouting();
 
